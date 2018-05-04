7621ec1254f0439a3ae3dfdd33f29e8db6af7a30
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2018-05-03 21:54:43 -0600

    javascript-1 formed

---
+++ javascript-1.js
@@ -0,0 +1,66 @@
+// ------------------------------------------- 0 -------------------------------- //
+// make a variable called `name`
+// on another line, set it to your name
+
+
+// create a function called `shout`
+// in that function, it will `console.log` the input into the function
+
+
+
+// pass your name into the function you made
+// if it outputs your name in all caps, you're good to go on
+
+
+
+
+
+
+
+
+
+// ------------------------------------------- 1 -------------------------------- //
+// now make another variable called `num` and set it to any number you choose
+
+
+
+
+// create a function called `add` that takes in two numbers
+// output from it those two numbers added together
+
+
+
+// create another function called `add5` that only accepts one number
+// output from it the result of `add` passing in the passed-in number and 5
+
+
+
+
+
+
+
+// console.log the output of `add5` called with your number
+// as long as it's your number plus 5, you're good to continue
+
+
+
+
+
+
+
+
+// ------------------------------------------- 2 -------------------------------- //
+// make another variable called `arr`
+// assign an array to it with all kinds of values, and as many as you want
+
+
+
+
+// console.log the length of the array (not by counting it yourself, use the special property on it)
+
+
+
+
+
+// log out the type of the first index of the array
+// * EXTRA CREDIT * use a javascript keyword to get the type instead of explicitly saying it
